32e4db164432d4e0affcdcd18175f4e15ae6aeb6
michaellucero <michaellucero@users.noreply.github.com>
2018-10-12 00:11:20 -0600

    Lesson04 Implements documents detail, listing, item, and adds drop down. Similar to contacts component with component communication.

--- src/app/documents/document-list/document-list.component.ts
+++ src/app/documents/document-list/document-list.component.ts
@@ -1,4 +1,5 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, EventEmitter, OnInit, Output } from '@angular/core';
+import { Document } from '../document.model';
 
 @Component({
   selector: 'app-document-list',
@@ -7,9 +8,53 @@ import { Component, OnInit } from '@angular/core';
 })
 export class DocumentListComponent implements OnInit {
 
+  @Output() selectedDocumentEvent = new EventEmitter();
+
+  documents: Document[] = [
+    new Document(
+      '1',
+      'Test Benchmark',
+      'Testing the level of starting knowledge in programing ',
+      'https://web.byui.edu/someDoc/test.doc',
+      null
+    ),
+    new Document(
+      '2',
+      'Intro',
+      'This is an intro lesson to programming principals',
+      'https://web.byui.edu/someDoc/Intro.doc',
+      null
+    ),
+    new Document(
+      '3',
+      'Section 2',
+      'This is the second lesson to programming principals',
+      'https://web.byui.edu/someDoc/section2.doc',
+      null
+    ),
+    new Document(
+      '4',
+      'Section 3',
+      'This is the third lesson to programming principals',
+      'https://web.byui.edu/someDoc/section3.doc',
+      null
+    ),
+    new Document(
+      '2',
+      'Final',
+      'This is the final lesson to programming principals',
+      'https://web.byui.edu/someDoc/final.doc',
+      null
+    ),
+  ];
+
   constructor() { }
 
   ngOnInit() {
+  }
+
+  onSelectedDocument(document: Document) {
+    this.selectedDocumentEvent.emit(document);
   }
 
 }
